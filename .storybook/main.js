6faf9e193907b148c5706c56e4ffcdd535d28ab4
Jake Motta <motta_jake@Yahoo.com>
2021-12-27 11:50:44 -0800

    AntD working, but with ignorable storybook error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../node_modules/storybook-addon-customize-antd-theme/dist/esm/stories/index.js"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    "storybook-addon-customize-antd-theme"
   ],
   "framework": "@storybook/react",
   "core": {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import 'antd/dist/antd.css';
+// import themeLess from '!!raw-loader!../src/theme/theme.less';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
